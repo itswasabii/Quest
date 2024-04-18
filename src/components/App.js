5fb6cb45f19a81747c4bf093d6ac97d291dcc726
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-04-19 00:15:31 +0300

    client commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import InterviewManager from "./InterviewManager";
 import UpdateInterview from "./UpdateInterview";
-
+import UserDashboard from "./UserDashboard";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={InterviewManager} />
           <Route path="/interviews/:id" component={UpdateInterview} />
+          <Route path="/dashboard/:id" component={UserDashboard} />
         </Switch>
       </div>
     </Router>

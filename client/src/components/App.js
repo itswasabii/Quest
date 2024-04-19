e030bc9d97d16a27384eb23cb1dedaabd0de9b69
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-04-19 10:43:05 +0300

    routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
 import ProfilePage from './ProfilePage';
 import Navbar from './Navbar';
 import AdminPanel from './AdminPanel';
 import UserDashboard from './UserDashboard';
+import UpdateInterview from './UpdateInterview';
 import LoginForm from './Login';
 
 function App() {
   const userId = 1; 
 
   return (
-    <div className="App">
-      <Navbar />
+    <Router>
+      <div className="App">
+        <Navbar />
 
+        {/* Use Switch to handle routing */}
+        <Switch>
+          {/* Route for registration form */}
+          <Route path="/register" component={RegistrationForm} />
+          
+          {/* Route for profile page */}
+          <Route path="/profile" component={ProfilePage} />
+          
+          {/* Route for updating interviews */}
+          <Route path="/interviews/:id" component={UpdateInterview} />
+
+          {/* Route for AdminPanel */}
+          <Route path="/admin" component={AdminPanel} />
+
+          {/* Define a default route for the home page */}
+          <Route path="/" exact>
+            <h2>Welcome to the Home Page</h2>
+            {/* Add other components or content for the home page here */}
+          </Route>
+        </Switch>
       {/* Use Switch to handle routing */}
       <Switch>
         {/* Route for registration form */}
@@ -32,6 +54,10 @@ function App() {
         </Route>
       </Switch>
 
+        {/* Render UserDashboard outside of the Switch */}
+        <UserDashboard userId={userId} />
+      </div>
+    </Router>
       {/* Render AdminPanel outside of the Switch */}
       { <AdminPanel /> }
       { <UserDashboard userId={userId} /> }

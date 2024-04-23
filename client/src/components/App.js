459481f7ea1505e5c558ac3b878935c0780e2469
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-04-24 00:50:18 +0300

    resoved more conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm';
 import ProfilePage from './ProfilePage';
 import Navbar from './Navbar';
-<<<<<<< HEAD
 import LoginForm from './Login';
 import HomePage from './HomePage.js'; // Import the HomePage component
 import Footer from './footer.js'; 
@@ -41,41 +40,6 @@ function App() {
             <Footer />
         </div>
     );
-=======
-import AdminPanel from './AdminPanel';
-import UserDashboard from './UserDashboard';
-import LoginForm from './Login';
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-
-      {/* Use Switch to handle routing */}
-      <Switch>
-        {/* Route for registration form */}
-        <Route path="/register" component={RegistrationForm} />
-        
-        {/* Route for profile page */}
-        <Route path="/profile" component={ProfilePage} />
-
-        {/* Route for profile page */}
-        <Route path="/LoginForm" component={LoginForm} />
-        
-        {/* Define a default route for the home page */}
-        <Route path="/" exact>
-          <h2>Welcome to the Home Page</h2>
-          {/* Add other components or content for the home page here */}
-        </Route>
-      </Switch>
-
-      {/* Render AdminPanel outside of the Switch */}
-      { <AdminPanel /> }
-      { <UserDashboard userId={userId} /> }
-
-    </div>
-  );
->>>>>>> main
 }
 
 export default App;

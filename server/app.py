70d6c6688122a838b64e56bf2df0b44492417cfb
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-04-24 13:15:53 +0300

    route edits

--- server/app.py
+++ server/app.py
@@ -373,11 +373,11 @@ def add_is_locked_column():
 
 @app.route('/')
 def serve_react_app():
-    return send_from_directory(app.static_folder, 'index.html')
+    return send_from_directory('static', 'index.html')
 
 @app.route('/static/<path:path>')
 def serve_static_files(path):
-    return send_from_directory(app.static_folder, path)
+    return send_from_directory('static', path)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5555, debug=True)
